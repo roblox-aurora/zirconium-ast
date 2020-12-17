2f4f804946e9bd2d5009cc7749637427aded83bd
Jonathan <void.knight09@gmail.com>
2020-12-17 19:29:54 +1300

    Updates to imports

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import * as ast from "./Nodes";
+import * as types from "./Nodes/Guards";
 import * as util from "Utility";
 import ZrLexer from "Lexer";
 import ZrTextStream from "TextStream";
 import ZrParser from "Parser";
 import ZrRichTextHighlighter from "Syntax/RichTextHighlighter";
+import * as factory from "./Nodes/Create";
 const AST_VERSION = PKG_VERSION;
 
-export { ZrLexer, ZrParser, ZrTextStream, ZrRichTextHighlighter, ast, AST_VERSION, util as astUtility };
+export { ZrLexer, ZrParser, ZrTextStream, ZrRichTextHighlighter, factory, types, AST_VERSION, util as astUtility };
